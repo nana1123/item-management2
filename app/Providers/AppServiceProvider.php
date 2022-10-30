ad0fc13ad85c3b4c64963b64974a3b2a8338b518
Kato Nana <nana.kato.nana@gmail.com>
2022-10-30 19:33:17 +0900

    4-2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(\APP::environment(['production'])){
+        if(config('app.env')==='production'){
             \URL::forceScheme('https');
         }
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
